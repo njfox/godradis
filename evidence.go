067fba73a8bd44365a4c2c19bdc464764108a10e
Nick Fox <nick@foxsec.net>
2019-06-17 15:23:12 -0400

    Add evidence methods to manipulate local evidence objects

--- evidence.go
+++ evidence.go
@@ -1,6 +1,10 @@
 package godradis
 
-import "github.com/iancoleman/orderedmap"
+import (
+	"errors"
+	"fmt"
+	"github.com/iancoleman/orderedmap"
+)
 
 type Evidence struct {
 	Id int `json:"id"`
@@ -14,4 +18,16 @@ type EvidenceIssue struct {
 	Id int `json:"id"`
 	Title string `json:"title"`
 	Url string `json:"url"`
+}
+
+func (e *Evidence) SetField(key, value string) {
+	e.Fields.Set(key, value)
+}
+
+func (e *Evidence) GetField(key string) (string, error) {
+	value, ok := e.Fields.Get(key)
+	if !ok {
+		return "", errors.New(fmt.Sprintf("field not found: %v", key))
+	}
+	return value.(string), nil
 }
