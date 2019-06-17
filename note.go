cd82b8fe720a4fc4c06e6d45e93d151e70786ebb
Nick Fox <nick@foxsec.net>
2019-06-17 15:22:52 -0400

    Add Note methods to manipulate local Note objects

--- note.go
+++ note.go
@@ -1,6 +1,10 @@
 package godradis
 
-import "github.com/iancoleman/orderedmap"
+import (
+	"fmt"
+	"github.com/iancoleman/orderedmap"
+	"github.com/pkg/errors"
+)
 
 type Note struct {
 	Id int `json:"id"`
@@ -9,4 +13,16 @@ type Note struct {
 	Fields orderedmap.OrderedMap `json:"fields"`
 	Text string `json:"text"`
 	Node *Node
+}
+
+func (n *Note) SetField(key, value string) {
+	n.Fields.Set(key, value)
+}
+
+func (n *Note) GetField(key string) (string, error) {
+	value, ok := n.Fields.Get(key)
+	if !ok {
+		return "", errors.New(fmt.Sprintf("field not found: %v", key))
+	}
+	return value.(string), nil
 }
